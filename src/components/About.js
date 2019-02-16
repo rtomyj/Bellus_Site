0e29169a88d77c27b180da763b5be206f7dd0727
Javi Gomez <rtomyj@gmail.com>
2019-02-16 12:07:36 -0600

    added a pic

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,18 @@ import PropTypes from 'prop-types'
 import { Typography } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 
+import roseImage from '../res/about_rose.png'
+
 const styles = {
 	summaryText: {
 		marginLeft: '10px',
 	},
 	heading: {
 		marginBottom: '5px'
+	},
+	roseImage: {
+		height: '350px',
+		width: '270px'
 	}
 }
 
@@ -27,8 +33,9 @@ class About extends Component {
 
 		return (
 		<div className={classes.text}>
+			<img src={roseImage} className={classes.roseImage}/>
 			<Typography variant='h4' className={classes.heading} >Rose Gomez (Artist)</Typography>
-			<Typography className={classes.summaryText} >Mother of three with years of experience with cosmetic products as well as cosmetology This lets us know whether the currently selected friend is online. If we pick a different friend and update the recipientID state variable, our useFriendStatus Hook will unsubscribe from the previously selected friend, and subscribe to the status of the newly selected one.</Typography>
+			<Typography className={classes.summaryText} >Mother of three with years of experience with cosmetic products as well as cosmetology. Starting as a cosmetics sales woman, I began professionally doing peoples make up to earn extra income. With seven plus years of experience I am the person to book for weddings, parties and anything else that needs an elegant touch.</Typography>
 		</div>
 		)
 	}
