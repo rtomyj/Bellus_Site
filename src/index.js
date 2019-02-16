1658af0b943cb8ff6134e973f23b8baad6b47f61
Javi Gomez <rtomyj@gmail.com>
2019-02-16 10:03:07 -0600

    optimized imports and seperated about us section to new component

--- src/index.js
+++ src/index.js
@@ -1,40 +1,25 @@
+import 'typeface-roboto'
+import './css/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types'
-import './css/index.css';
 
 import NavBar from './components/NavBar.js'
-import { Typography } from '@material-ui/core';
-
-import { withStyles } from '@material-ui/core/styles';
+import About from './components/About.js'
 
 
-
-const styles = {
-	summaryText: {
-		marginLeft: '15px'
-	}
-}
 
 class Home extends React.Component{
 
 	render() {
-		const { classes } = this.props
 		return(
-			<div styles='max-width: 1500px; margin: auto;'>
+			<div >
 				<NavBar />
 				<br />
-				<Typography variant='h4'>Rose Gomez (Artist)</Typography>
-				<Typography className={classes.summaryText} >Mother of three with years of experience with cosmetic products as well as cosmetology </Typography>
+
+				<About />
 			</div>
 		)
 	}
 }
 
 ReactDOM.render(<Home />, document.getElementById('root'))
-
-Home.PropTypes = {
-	classes: PropTypes.object.isRequired
-}
-
-export default withStyles(styles)(Home)
