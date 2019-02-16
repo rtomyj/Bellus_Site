a5b647233ce73e48d86af92d38d66f27d3a48558
Javi Gomez <rtomyj@gmail.com>
2019-02-16 01:33:15 -0600

    added new content

--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import PropTypes from 'prop-types'
+import './css/index.css';
 
-import NavBar from './NavBar.js'
+import NavBar from './components/NavBar.js'
+import { Typography } from '@material-ui/core';
 
+import { withStyles } from '@material-ui/core/styles';
+
+
+
+const styles = {
+	summaryText: {
+		marginLeft: '15px'
+	}
+}
 
 class Home extends React.Component{
 
 	render() {
+		const { classes } = this.props
 		return(
-			<NavBar />
+			<div styles='max-width: 1500px; margin: auto;'>
+				<NavBar />
+				<br />
+				<Typography variant='h4'>Rose Gomez (Artist)</Typography>
+				<Typography className={classes.summaryText} >Mother of three with years of experience with cosmetic products as well as cosmetology </Typography>
+			</div>
 		)
 	}
 }
 
 ReactDOM.render(<Home />, document.getElementById('root'))
 
-/*
-ReactDOM.render(<App />, document.getElementById('root'));
+Home.PropTypes = {
+	classes: PropTypes.object.isRequired
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
-*/
+export default withStyles(styles)(Home)
