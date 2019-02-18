2a8c5bcff2873a87f49f1d68811c32d7ceb40714
Javi Gomez <rtomyj@gmail.com>
2019-02-17 21:35:05 -0600

    New about us, new style, new colors

--- src/components/About.js
+++ src/components/About.js
@@ -2,21 +2,28 @@ import 'typeface-roboto'
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
-import { Typography } from '@material-ui/core';
+import { Typography, Grid } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 
 import roseImage from '../res/about_rose.png'
+import { yellow, deepOrange } from '@material-ui/core/colors';
 
 const styles = {
 	summaryText: {
-		marginLeft: '10px',
 	},
 	heading: {
-		marginBottom: '5px'
+		marginBottom: '15px',
+		color: deepOrange['300']
+	},
+	firstHeading: {
+		margin: '8px'
+	},
+	otherHeading: {
+		marginTop: '65px'
 	},
 	roseImage: {
-		height: '350px',
-		width: '270px'
+		height: '600px',
+		align: 'center',
 	}
 }
 
@@ -31,11 +38,38 @@ class About extends Component {
 	render() {
 		const { classes } = this.props
 
+		const temp = {
+			marginTop: '300px'
+		}
+
 		return (
 		<div className={classes.text}>
 			<img src={roseImage} className={classes.roseImage}/>
-			<Typography variant='h4' className={classes.heading} >Rose Gomez (Artist)</Typography>
-			<Typography className={classes.summaryText} >Mother of three with years of experience with cosmetic products as well as cosmetology. Starting as a cosmetics sales woman, I began professionally doing peoples make up to earn extra income. With seven plus years of experience I am the person to book for weddings, parties and anything else that needs an elegant touch.</Typography>
+			
+			<Grid container direction='row' xs='10' className={classes.otherHeading}>
+				<Grid item xs='3'>
+					<Typography 
+						variant='h4' 
+						className={[classes.heading, classes.firstHeading].join(' ')} >
+						Rose Gomez - The Artist
+					</Typography>
+				</Grid>
+
+				<Grid item xs>
+					<Typography 
+						className={classes.summaryText} 
+						variant='subtitle1' >
+						Mother of three with years of experience with cosmetic products as well as cosmetology. Starting as a cosmetics sales woman, I began professionally doing peoples make up to earn extra income. With seven plus years of experience I am the person to book for weddings, parties and anything else that needs an elegant touch.
+					</Typography>
+				</Grid>
+
+			</Grid>
+
+			<Typography 
+				variant='h4' 
+				className={[classes.heading, classes.otherHeading].join(' ')} >
+				Contact Us
+			</Typography>
 		</div>
 		)
 	}
